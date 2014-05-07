d230bd0a8d32d913b188d6ce6d54986e7f2f4396
Nick Reuter <nreuter85@gmail.com>
2014-05-07 03:15:12 -0500

    msm7x30-common: add gralloc_priv header
     * override the one in hardware to fix issues with our camera
    
    Change-Id: Icd620a7402867974d82a4c3e55d1f2b58c853ba7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
+
 # Common stuff
 DEVICE_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
 
